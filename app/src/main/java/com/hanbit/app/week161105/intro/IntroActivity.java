a6244d10c9eb10e28eddaf84da1a59f97138a101
pakjkwan2 <pakjkwan2@gmail.com>
2016-11-19 14:35:42 +0900

    :161119E 14:00

---
+++ app/src/main/java/com/hanbit/app/week161105/intro/IntroActivity.java
@@ -0,0 +1,15 @@
+package com.hanbit.app.week161105.intro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hanbit.app.week161105.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
